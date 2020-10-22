d488f59bbd0e09d04f053084fc090e511d60e6f7
Olldriver <54601328+Olldriver@users.noreply.github.com>
2020-10-22 18:41:37 +0300

    slider is ended

--- script.js
+++ script.js
@@ -1,54 +1,68 @@
-
-
 //SELECTORS
 
 let slides = document.querySelectorAll(".services__card"); //object of cards
-console.log(slides);
 let leftButton = document.querySelector(".services__leftbutton")
 let rightButton = document.querySelector(".services__rightbutton")
-
-
-
+let dots = document.querySelector(".services__dots")
 let currentSlide = 0;
 
 //FUNCTIONS
 
+function createDot() {
+    for (let i = 0; i < slides.length; i++) {
+        dots.insertAdjacentHTML('beforeend', `<i class="services__dot" data-dot=${i}></i>`)
+    }
+}
+createDot()
+
+function checkdot(e) {
+    if (e.target.classList.value != 'services__dot') return
+    deleteActiveDots()
+    e.target.classList.add('services__dot__active')
+    deleteActiveClassInSlides()
+    slides[e.target.dataset.dot].classList.add("services__active");
+    currentSlide = e.target.dataset.dot
+}
+
+function deleteActiveDots() {
+    for (let index = 3; index < slides.length + 3; index++) {
+        dots.childNodes[index].classList.remove('services__dot__active')
+    }
+}
+
 function deleteActiveClassInSlides() {
-
-
-    // console.log(item)
-    // console.log(slides[item])
     slides[currentSlide].classList.remove("services__active");
-
-
-
 }
 
 function addClassToprevSlide() {
     if (currentSlide == 0) {
-        currentSlide = 2;
+        currentSlide = slides.length - 1;
     } else {
         currentSlide--;
     }
     slides[currentSlide].classList.add("services__active");
+    dots.childNodes[currentSlide + 3].classList.add('services__dot__active')
 }
 
 function addClassTonextSlide() {
-    if (currentSlide == 2) {
+    if (currentSlide == slides.length - 1) {
         currentSlide = 0;
     } else {
         currentSlide++;
     }
     slides[currentSlide].classList.add("services__active");
+    dots.childNodes[currentSlide + 3].classList.add('services__dot__active')
 }
 
 function prevSlide() {
     deleteActiveClassInSlides()
+    deleteActiveDots()
     addClassToprevSlide()
 }
 
 function nextSlide() {
     deleteActiveClassInSlides()
+    deleteActiveDots()
     addClassTonextSlide()
 }
 
@@ -56,3 +70,4 @@ function nextSlide() {
 
 leftButton.addEventListener("click", prevSlide)
 rightButton.addEventListener("click", nextSlide)
+dots.addEventListener("click", checkdot)
